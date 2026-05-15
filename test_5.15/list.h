f44a02185f669eb31ec295468d872be205948889
191599805 <191599805@qq.com>
2026-05-16 00:26:12 +0800

    list类基础实现 1

--- test_5.15/list.h
+++ test_5.15/list.h
@@ -10,11 +10,17 @@ namespace QzQz
 	struct list_node
 	{
 		typedef list_node<T> Node;
-		T _data = 0;
+		T _data;
 		Node* _next;
 		Node* _prev;
 
-		list_node() { }
+		list_node(const T& x = T()) : _data(x){ }
+	};
+
+	template<class T>
+	struct list_iterator
+	{
+		typedef list_iterator<T>* iterator;
 	};
 
 	template<class T>
@@ -22,6 +28,8 @@ namespace QzQz
 	{
 		typedef list_node<T> Node;
 	public:
+		typedef list_iterator<T>* iterator;
+
 		list()
 		{
 			Node* tmp = new Node;
@@ -29,19 +37,40 @@ namespace QzQz
 			_head->_next = _head->_prev = _head;
 		}
 
-		void push_back(T& x)
+		void push_back(const T& x)
 		{
-			;
+			Node* tmp = new Node(x);
+			Node* tail = _head->_prev;
+
+			tail->_next = tmp;
+			tmp->_prev = tail;
+			tmp->_next = _head;
+			_head->_prev = tmp;
+
+			++_size;
+		}
+
+		size_t size()
+		{
+			return _size;
 		}
 
 	private:
 		Node* _head;
+		size_t _size = 0;
 	};
 
 
 	void test_list1()
 	{
 		list<int> lt;
+
+		lt.push_back(1);
+		lt.push_back(2);
+		lt.push_back(3);
+		lt.push_back(4);
+
+		std::cout << lt.size() << std::endl;
 	}
 
 }
